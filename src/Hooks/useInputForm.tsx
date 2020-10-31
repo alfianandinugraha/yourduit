20d90ef15238bb51b35596794884ae1680164991
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-31 16:35:33 +0700

    add: create functional useInputForm

--- src/Hooks/useInputForm.tsx
+++ src/Hooks/useInputForm.tsx
@@ -1,4 +1,11 @@
-const useInputForm = () => {
+import { useState } from 'react'
+
+const useInputForm = (): [string, (e: any) => void] => {
+  const [value, setValue] = useState<string>("")
+
+  const updateValue = (e: any) => setValue(e.target.value)
+
+  return [value, updateValue]
 }
 
 export default useInputForm
