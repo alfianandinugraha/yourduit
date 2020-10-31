f5d310b3da5f0c72cb7fe7ba434ba2c73a9dfc14
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-31 15:56:32 +0700

    add: create a background and logo

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,7 +1,13 @@
 import React from 'react'
 
 const LoginPage = () => {
-  return (null)
+  return (
+    <div className="bg-primary w-100 d-flex text-white" style={{height: '100vh'}}>
+      <div className="m-auto">
+        <h1>yourDuit</h1>
+      </div>
+    </div>
+  )
 }
 
 export default LoginPage
