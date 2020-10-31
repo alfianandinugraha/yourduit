01ecbcac577011bf0855394b80b086643372dba5
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-31 16:06:45 +0700

    add: create form to login

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Container, Row, Col } from 'react-bootstrap'
+import { Container, Row, Col, Form } from 'react-bootstrap'
 
 const LoginPage = () => {
   return (
@@ -8,6 +8,21 @@ const LoginPage = () => {
         <Row>
           <Col sm="12">
             <h1 className="text-center">yourDuit</h1>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col sm="12" className="text-center">
+            <div>Insert your name</div>
+            <Form.Control type="text" placeholder="John Doe" className="mt-2"/>
+          </Col>
+        </Row>
+        <Row className="mt-4">
+          <Col sm="12" className="text-center">
+            <div>Select your currency</div>
+            <Form.Control as="select" className="mt-2">
+              <option>US Dollar</option>
+              <option>Indonesia Rupiah</option>
+            </Form.Control>
           </Col>
         </Row>
       </Container>
