913d2e8cb8e8de8a20b2ef16b5b43f61868d50b6
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-10-31 15:59:42 +0700

    add: add container, row, and col for layout

--- src/Pages/LoginPage.tsx
+++ src/Pages/LoginPage.tsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { Container, Row, Col } from 'react-bootstrap'
 
 const LoginPage = () => {
   return (
-    <div className="bg-primary w-100 d-flex text-white" style={{height: '100vh'}}>
-      <div className="m-auto">
-        <h1>yourDuit</h1>
-      </div>
+    <div className="bg-primary w-100 d-flex text-white" style={{ height: '100vh' }}>
+      <Container className="m-auto">
+        <Row>
+          <Col sm="12">
+            <h1 className="text-center">yourDuit</h1>
+          </Col>
+        </Row>
+      </Container>
     </div>
   )
 }
