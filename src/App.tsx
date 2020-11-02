6cc7371a006837cd930f0d469414d195a98b490e
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 12:50:49 +0700

    add: make simple route and make route to /dashboard

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
+import { Route, Switch } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
 
 function App() {
   return (
     <>
-      <LoginPage />
+      <Switch>
+        <Route exact path="/">
+          <LoginPage />
+        </Route>
+        <Container>
+          <Route exact path="/dashboard">
+            <h1>Hello this is dashboard !</h1>
+          </Route>
+        </Container>
+      </Switch>
     </>
   );
 }
