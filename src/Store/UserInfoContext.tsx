8011bd5caf19a1b04b9e602896f1afb7467cc7c4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 12:58:02 +0700

    add: create context and interface for UserInfo

--- src/Store/UserInfoContext.tsx
+++ src/Store/UserInfoContext.tsx
@@ -1,5 +1,23 @@
 import React from 'react'
 
+interface UserInfo {
+  name: string;
+  currency: string;
+}
+
+interface UserInfoStore {
+  userInfo: UserInfo;
+  setUserInfo: (userInfo: UserInfo) => void
+}
+
+export const userInfoContext = React.createContext<UserInfoStore>({
+  userInfo: {
+    name: "Guest",
+    currency: "$"
+  },
+  setUserInfo: () => {}
+})
+
 const UserInfoContext = () => {
   return (
     <div>
