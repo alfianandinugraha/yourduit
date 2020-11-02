80470bfd41c7276cc5f3b8ef5c24491dbbb8fbdb
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 15:06:52 +0700

    add: adding border left

--- src/Components/CashBoard.tsx
+++ src/Components/CashBoard.tsx
@@ -13,9 +13,10 @@ export const CashBoard = (props: Props) => {
         backgroundColor: "white",
         borderRadius: "5px",
         marginBottom: "1rem",
-        boxShadow: "0px 3px 6px 0 rgb(00,00,00, 0.16)"
+        boxShadow: "0px 3px 6px 0 rgb(00,00,00, 0.16)",
+        borderLeft: `14px solid`
       }
-    }>
+    } className={`border-${props.color}`}>
       <div style={
         {
           padding: "25px 0 25px 19px"
