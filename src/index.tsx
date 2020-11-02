45715cdc2a02a38189886d152153a4d99597b368
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 12:50:32 +0700

    add: make App as child of HashRouter

--- src/index.tsx
+++ src/index.tsx
@@ -1,13 +1,16 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter} from 'react-router-dom'
 import reportWebVitals from './reportWebVitals';
 
 import './Style/Index.scss'
 
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <HashRouter>
+      <App />
+    </HashRouter>
   </React.StrictMode>,
   document.getElementById('root')
 );
