6913429797fa1979761d82c32b4b04b60edb15b3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 13:12:00 +0700

    add: register Store on App and make it as root

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,23 @@ import React from 'react';
 import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
 import LoginPage from './Pages/LoginPage';
+import { Store } from './Store/Store';
 
 function App() {
   return (
     <>
-      <Switch>
-        <Route exact path="/">
-          <LoginPage />
-        </Route>
-        <Container>
-          <Route exact path="/dashboard">
-            <h1>Hello this is dashboard !</h1>
+      <Store>
+        <Switch>
+          <Route exact path="/">
+            <LoginPage />
           </Route>
-        </Container>
-      </Switch>
+          <Container>
+            <Route exact path="/dashboard">
+              <h1>Hello this is dashboard !</h1>
+            </Route>
+          </Container>
+        </Switch>
+      </Store>
     </>
   );
 }
