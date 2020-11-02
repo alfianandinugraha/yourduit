b72a6d1e549019a4affe531f73bfad00eb64fc50
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 13:58:17 +0700

    add: create interface Activity and ActivityContext

--- src/Store/ActivityStore.tsx
+++ src/Store/ActivityStore.tsx
@@ -4,6 +4,20 @@ interface Props {
   children: ReactNode
 }
 
+interface Activity {
+  id: number;
+  description: string;
+  nominal: number;
+  type: "0" | "1";
+  createdAt: number;
+  updatedAt: number;
+}
+
+interface ActivityContext {
+  activities: Activity[];
+  setActivities: (activity: Activity[]) => void
+}
+
 const ActivityStore = (props: Props) => {
   return (
     <div>
