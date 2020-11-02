ad80470f649604bfd1534bce3fe91c20369d7292
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 13:54:13 +0700

    add: create file for ActivityStore.tsx

---
+++ src/Store/ActivityStore.tsx
@@ -0,0 +1,13 @@
+import React, { ReactNode } from 'react'
+
+interface Props {
+  children: ReactNode
+}
+
+export const ActivityStore = (props: Props) => {
+  return (
+    <div>
+      {props.children}
+    </div>
+  )
+}
