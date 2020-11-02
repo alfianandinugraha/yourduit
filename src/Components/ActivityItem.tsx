9677b99b33b329bfbf05deaa921236865833d940
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 16:01:44 +0700

    add: make component for ActivityItem

---
+++ src/Components/ActivityItem.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import {Activity} from '../Store/ActivityStore'
+
+const ActivityItem = (props: Activity) => {
+  return (
+    <div className="d-flex mb-2" key={props.id}>
+      <div>{props.description}</div>
+      <div className={`ml-auto text-${props.type === "1" ? "danger" : "primary"}`}>{props.nominal}</div>
+    </div>
+  )
+}
+
+export default ActivityItem
