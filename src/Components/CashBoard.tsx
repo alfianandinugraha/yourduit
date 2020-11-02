478ef5d75171527430924c2df4bad0ef43e1d4d4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 14:43:10 +0700

    add: create styling for box on CashBoard

--- src/Components/CashBoard.tsx
+++ src/Components/CashBoard.tsx
@@ -7,9 +7,22 @@ interface Props {
 
 export const CashBoard = (props: Props) => {
   return (
-    <div>
-      <div>Spending this month</div>
-      <h1>{props.nominal}</h1>
+    <div style={
+      {
+        backgroundColor: "white",
+        borderRadius: "5px",
+        marginBottom: "1rem",
+        boxShadow: "0px 3px 6px 0 rgb(00,00,00, 0.16)"
+      }
+    }>
+      <div style={
+        {
+          padding: "25px 0 25px 19px"
+        }
+      }>
+        <div>Spending this month</div>
+        <h1>{props.nominal}</h1>
+      </div>
     </div>
   )
 }
