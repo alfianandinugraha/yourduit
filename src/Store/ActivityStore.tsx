318d6e97dd00062486fdf1baee25477cf571228c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-02 14:09:15 +0700

    add: create dummy activities as initial value

--- src/Store/ActivityStore.tsx
+++ src/Store/ActivityStore.tsx
@@ -19,7 +19,32 @@ interface ActivityContext {
 }
 
 const activityStoreInitialValue: ActivityContext = {
-  activities: [],
+  activities: [
+    {
+      id: 3,
+      description: "Beli Seblak",
+      nominal: 200000,
+      type: "0",
+      createdAt: 3,
+      updatedAt: 3
+    },
+    {
+      id: 2,
+      description: "Gajian",
+      nominal: 1500000,
+      type: "1",
+      createdAt: 2,
+      updatedAt: 3
+    },
+    {
+      id: 1,
+      description: "Beli kursus online",
+      nominal: 500000,
+      type: "0",
+      createdAt: 1,
+      updatedAt: 3
+    },
+  ],
   setActivities: () => {}
 }
 
