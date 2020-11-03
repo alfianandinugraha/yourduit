9219925fd2e66d13fd75d64ada27cdae94dd3149
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-03 17:45:58 +0700

    add: adding cross button for close form

--- src/Components/ActivityForm.tsx
+++ src/Components/ActivityForm.tsx
@@ -1,8 +1,9 @@
 import React, { useContext } from 'react'
 import { Col, Container, Row } from 'react-bootstrap';
+import { PlusIcon } from '../Common/PlusIcon';
 import { Activity } from '../Store/ActivityStore'
 import { themeContext } from '../Store/ThemeStore'
-import { ActivityFormTitle, ActivityFormWrapper } from '../Style/Styled';
+import { ActivityFormTitle, ActivityFormWrapper, Circle } from '../Style/Styled';
 
 interface Props {
   activity?: Activity
@@ -27,8 +28,15 @@ export const ActivityForm = (props: Props = {activity: defaultActivity}) => {
           <ActivityFormWrapper className="position-fixed w-100">
             <Container>
               <Row>
-                <Col>
+                <Col className="d-flex justify-content-between">
                   <ActivityFormTitle>Add Activity</ActivityFormTitle>
+                  <Circle
+                    width="30.87px"
+                    height="30.87px"
+                    className="bg-danger d-flex justify-content-center align-items-center cursor-pointer"
+                  >
+                    <PlusIcon width="13px" height="13px"/>
+                  </Circle>
                 </Col>
               </Row>
             </Container>
