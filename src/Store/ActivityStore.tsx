7382225abb0653d831835aa536575b505246320a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-03 16:54:17 +0700

    add: getTotalNominalThisMonth for calculate summary cash this month

--- src/Store/ActivityStore.tsx
+++ src/Store/ActivityStore.tsx
@@ -23,6 +23,7 @@ interface ActivityContext {
   activities: Activity[];
   setActivities: (activity: Activity[]) => void;
   getTotalNominal: (from?: number, to?: number) => TotalNominalType;
+  getTotalNominalThisMonth: () => TotalNominalType;
 }
 
 const activityStoreInitialValue: ActivityContext = {
@@ -33,7 +34,7 @@ const activityStoreInitialValue: ActivityContext = {
       nominal: 200000,
       type: "0",
       createdAt: 3,
-      updatedAt: 3
+      updatedAt: 1604250000000
     },
     {
       id: 2,
@@ -41,7 +42,7 @@ const activityStoreInitialValue: ActivityContext = {
       nominal: 1500000,
       type: "1",
       createdAt: 2,
-      updatedAt: 3
+      updatedAt: 1604163600000
     },
     {
       id: 1,
@@ -49,11 +50,18 @@ const activityStoreInitialValue: ActivityContext = {
       nominal: 500000,
       type: "0",
       createdAt: 1,
-      updatedAt: 3
+      updatedAt: 1603558800000
     },
   ],
   setActivities: () => { },
   getTotalNominal: (from: number = 0, to: number = 0): TotalNominalType => {
+    return {
+      income: 0,
+      spending: 0,
+      summary: 0
+    }
+  },
+  getTotalNominalThisMonth: (from: number = 0, to: number = 0): TotalNominalType => {
     return {
       income: 0,
       spending: 0,
@@ -71,8 +79,8 @@ const ActivityStore = (props: Props) => {
   )
 
   const getTotalNominal = (
-    from: number = activities[0].updatedAt,
-    to: number = activities[activities.length - 1].updatedAt
+    from: number = activities[activities.length - 1].updatedAt,
+    to: number = activities[0].updatedAt
   ): TotalNominalType => {
     let income = 0;
     let spending = 0;
@@ -98,12 +106,28 @@ const ActivityStore = (props: Props) => {
     return {income, spending, summary}
   }
 
+  const getTotalNominalThisMonth = (): TotalNominalType => {
+    const thisTime = {
+      days: 1,
+      months: new Date().getMonth(),
+      years: new Date().getFullYear()
+    }
+  
+    const millisecondFromFirstDate = new Date(thisTime.years, thisTime.months, thisTime.days).getTime();
+    const millisecondFromNow = new Date().getTime()
+
+    const totalNominal = getTotalNominal(millisecondFromFirstDate, millisecondFromNow)
+
+    return totalNominal
+  }
+
   return (
     <activityContext.Provider value={
       {
         activities: activities,
         setActivities: setActivities,
-        getTotalNominal
+        getTotalNominal,
+        getTotalNominalThisMonth
       }
     }>
       {props.children}
