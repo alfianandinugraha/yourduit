0051c7efb9b8548be54e949e751a92a01c0a9577
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-03 11:36:58 +0700

    add: create IncludeCashSummary for add CashSummary to children

--- src/Components/CashSummary.tsx
+++ src/Components/CashSummary.tsx
@@ -1,13 +1,23 @@
-import React from 'react'
+import React, { ReactNode } from 'react'
+import { CashSummaryWrapper } from '../Style/Styled'
 
 interface Props {
-  nominal: string;
+  nominal: number;
 }
 
 export const CashSummary = (props: Props) => {
   return (
-    <div>
-      
-    </div>
+    <CashSummaryWrapper>
+      <h1>this is cash summary</h1>
+    </CashSummaryWrapper>
+  )
+}
+
+export const IncludeCashSummary = ({ children }: {children: ReactNode}) => {
+  return (
+    <>
+      {children}
+      <CashSummary nominal={700000}/>
+    </>
   )
 }
