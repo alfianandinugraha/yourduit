7e0958e466a69abed299a8750f886ed43ba2d6ba
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-03 10:30:25 +0700

    add: create styled component for GreetingDashboard

--- src/Pages/DashboardPage.tsx
+++ src/Pages/DashboardPage.tsx
@@ -3,27 +3,22 @@ import { Col, Row } from 'react-bootstrap'
 import ActivityItem from '../Components/ActivityItem'
 import { CashBoard } from '../Components/CashBoard'
 import { activityContext } from '../Store/ActivityStore'
-import { BackgroundHero } from '../Style/Styled'
+import { BackgroundHero, GreetingDashboard } from '../Style/Styled'
 
 const DashboardPage = () => {
   const { activities } = useContext(activityContext)
 
   return (
     <>
-      <BackgroundHero className="bg-primary w-100 position-absolute" height="406px"/>
-      <Row style={
-        {
-          marginTop: "79px",
-          color: "white"
-        }
-      }>
+      <BackgroundHero className="bg-primary w-100 position-absolute" height="406px" />
+      <GreetingDashboard>
         <Col sm="12">
           <span>Welcome,</span>
         </Col>
         <Col sm="12">
           <h1>Alfian Andi</h1>
         </Col>
-      </Row>
+      </GreetingDashboard>
       <Row>
         <Col md="6">
           <CashBoard nominal={700000} text="Spending this month" color="danger" currency="Rp"/>
--- src/Style/Styled.ts
+++ src/Style/Styled.ts
@@ -1,3 +1,4 @@
+import { Row } from 'react-bootstrap'
 import styled from 'styled-components'
 
 interface BackgroundHeroProps {
@@ -10,4 +11,9 @@ export const BackgroundHero = styled.div<BackgroundHeroProps>`
   left: 0;
   border-radius: 0 0 20px 20px;
   z-index: -1;
+`
+
+export const GreetingDashboard = styled(Row)`
+  margin-top: 79px;
+  color: white;
 `
