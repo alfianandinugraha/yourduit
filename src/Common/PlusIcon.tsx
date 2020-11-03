c25538c3b7ff637b1a8ca95104d3744ba3692b6c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-03 12:02:23 +0700

    add: adding PlusIcon

---
+++ src/Common/PlusIcon.tsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+export const PlusIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="39" height="39" viewBox="0 0 39 39">
+      <g id="plus" transform="translate(-168.5 -742)">
+        <rect id="Rectangle_9" data-name="Rectangle 9" width="4" height="39" rx="2" transform="translate(186 742)" fill="#fff" />
+        <rect id="Rectangle_10" data-name="Rectangle 10" width="4" height="39" rx="2" transform="translate(168.5 763.5) rotate(-90)" fill="#fff" />
+      </g>
+    </svg>
+  )
+}
+
+
+
