c0196fb54bd58347b30a5cd18a26ab5ef2f39966
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-03 15:46:03 +0700

    add: create file ActivityForm.tsx

---
+++ src/Components/ActivityForm.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Activity } from '../Store/ActivityStore'
+
+export const ActivityForm = (activity: Activity) => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
