d34b766f3b52eac1a9c9b6af5cb1a36e35b8d96c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-04 14:18:33 +0700

    add: create event when backdrop clicked

--- src/Components/Backdrop.tsx
+++ src/Components/Backdrop.tsx
@@ -2,14 +2,18 @@ import React, { useContext } from 'react'
 import { themeContext } from '../Store/ThemeStore'
 import { BackdropWrapper } from '../Style/Styled'
 
-export const Backdrop = () => {
+interface Props {
+  onClick: (e: React.MouseEvent<HTMLElement>) => void
+}
+
+export const Backdrop = (props: Props) => {
   const {isBackdropShow} = useContext(themeContext)
 
   return (
-    <>
+    <div onClick={props.onClick}>
       {
         isBackdropShow ? <BackdropWrapper /> : null 
       } 
-    </>
+    </div>
   )
 }
