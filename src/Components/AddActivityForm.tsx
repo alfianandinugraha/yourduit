c9621f26c309588533c7e030bf4e8b72d6faa705
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-04 17:42:38 +0700

    add: implement feature to save on localstorage

--- src/Components/AddActivityForm.tsx
+++ src/Components/AddActivityForm.tsx
@@ -1,10 +1,17 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { activityContext } from '../Store/ActivityStore'
 import { ActivityForm } from './ActivityForm'
+import { saveActivityToLocalStorage } from '../Functions/LocalStorage'
 
 export const AddActivityForm = () => {
+  const { addActivity } = useContext(activityContext)
+
   return (
     <>
-      <ActivityForm getPayload={(payload) => console.log(payload)}/>
+      <ActivityForm getPayload={(payload) => {
+        addActivity(payload)
+        saveActivityToLocalStorage(payload)
+      }}/>
     </>
   )
 }
