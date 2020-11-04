337817709e707a8ade9d4b18d9d9eb61c96e2a62
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-04 18:29:20 +0700

    add: fetchAllActivityFromLocalStorage to fetch all localstorage

--- src/Functions/LocalStorage.tsx
+++ src/Functions/LocalStorage.tsx
@@ -9,3 +9,31 @@ export const saveActivityToLocalStorage = (activity: Activity) => {
     `${activity.id}${separator}${activity.description}${separator}${activity.nominal}${separator}${activity.type}${separator}${activity.createdAt}${separator}${activity.updatedAt}`
   )
 }
+
+export const fetchAllActivityFromLocalStorage = () => {
+  const keyActivity = LOCAL_STORAGE_PREFIX + LOCAL_STORAGE_ACTIVITY_SEPARATOR + 'activity';
+  const activities: Activity[] = []
+
+  for (let index = 0; index < localStorage.length; index++) {
+    const element = localStorage.key(index)
+    
+    if (element?.startsWith(keyActivity)) {
+      const [id, description, nominal, type, createdAt, updatedAt] = localStorage.getItem(element)?.split(LOCAL_STORAGE_ACTIVITY_SEPARATOR) as string[]
+
+      let typeResult: "1" | "0" = type === "1" ? "1" : "0" 
+
+      const activity: Activity = {
+        id: +id,
+        description,
+        nominal: +nominal,
+        type: typeResult,
+        createdAt: +createdAt,
+        updatedAt: +updatedAt
+      }
+
+      activities.push(activity)
+    }
+  }
+
+  return activities
+}
