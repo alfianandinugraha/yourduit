320ca3db0ac868a5de0b4abf4c9c2ffcfb2c2ee4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-04 09:55:30 +0700

    add: register AuthStore to Store

--- src/Store/Store.tsx
+++ src/Store/Store.tsx
@@ -1,5 +1,6 @@
 import React, { ReactNode } from 'react'
 import ActivityStore from './ActivityStore'
+import { AuthStore } from './AuthStore'
 import { ThemeStore } from './ThemeStore'
 import UserInfoContext from './UserInfoContext'
 
@@ -10,13 +11,15 @@ interface Props {
 export const Store = (props: Props) => {
   return (
     <>
-      <UserInfoContext>
-        <ActivityStore>
-          <ThemeStore>
-            {props.children}
-          </ThemeStore>
-        </ActivityStore>
-      </UserInfoContext>
+      <AuthStore>
+        <UserInfoContext>
+          <ActivityStore>
+            <ThemeStore>
+              {props.children}
+            </ThemeStore>
+          </ActivityStore>
+        </UserInfoContext>
+      </AuthStore>
     </>
   )
 }
