a137ccf9f72a28c9da1c3f6ff7fac1da4c02ca92
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-07 14:08:08 +0700

    add: show nominal activity

--- src/Pages/ActivityPage.tsx
+++ src/Pages/ActivityPage.tsx
@@ -3,11 +3,13 @@ import { Col, Row } from 'react-bootstrap'
 import { useParams } from 'react-router'
 import { LayoutProtectedPage } from '../Layout/LayoutProtectedPage'
 import { Activity, activityContext } from '../Store/ActivityStore'
+import { userInfoContext } from '../Store/UserInfoContext'
 import { BackgroundHero } from '../Style/Styled'
 
 export const ActivityPage = () => {
   const { id }: { id: string } = useParams()
   const { getActivityById } = useContext(activityContext)
+  const { userInfo } = useContext(userInfoContext)
   const [activity] = useState<Activity>(getActivityById(+id))
   const date = {
     days: new Date(activity.updatedAt).getDay() + 1,
@@ -18,12 +20,18 @@ export const ActivityPage = () => {
   return (
     <LayoutProtectedPage>
       <BackgroundHero className="bg-primary w-100 position-absolute" height="106px"/>
-      <Row className="text-white bg-primary pb-3" style={{ borderRadius: "0 0 20px 20px"}}>
+      <Row className="text-white bg-primary pb-3 mb-3" style={{ borderRadius: "0 0 20px 20px"}}>
         <Col className="col-12">
           <h1>{activity.description}</h1>
         </Col>
         <Col>
           <span>{`${date.months}/${date.days}/${date.years}`}</span>
+        </Col>
+      </Row>
+      <Row>
+        <Col className={`d-flex text-${activity.type === "1" ? "primary" : "danger"}`}>
+          <span>{userInfo.location.currency}</span>
+          <h1>{new Intl.NumberFormat(userInfo.location.locale).format(activity.nominal)}</h1>
         </Col>
       </Row>
     </LayoutProtectedPage>
