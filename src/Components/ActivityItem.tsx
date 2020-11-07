96481cedc399bfd66529b86f40625517c4ec554a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-07 10:46:40 +0700

    add: adding number format

--- src/Components/ActivityItem.tsx
+++ src/Components/ActivityItem.tsx
@@ -1,15 +1,19 @@
-import React, { useContext } from 'react'
+import React, { useContext, useState } from 'react'
 import {Activity} from '../Store/ActivityStore'
 import { userInfoContext } from '../Store/UserInfoContext'
 
 const ActivityItem = (props: Activity) => {
   const { userInfo } = useContext(userInfoContext)
-  const { location } = userInfo
+  const [nominal] = useState<string>(new Intl.NumberFormat(userInfo.location.locale).format(props.nominal))
 
   return (
     <div className="d-flex mb-2">
       <div>{props.description}</div>
-      <div className={`ml-auto text-${props.type === "1" ? "primary" : "danger"}`}>{location.currency}{props.nominal}</div>
+      <div
+        className={`ml-auto text-${props.type === "1" ? "primary" : "danger"}`}
+      >
+        {userInfo.location.currency}{nominal}
+      </div>
     </div>
   )
 }
