1c6d636a17e21775809af08dfef1c315ea8de98d
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-07 10:37:45 +0700

    add: adding currency depends on user info

--- src/Components/ActivityItem.tsx
+++ src/Components/ActivityItem.tsx
@@ -1,11 +1,15 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import {Activity} from '../Store/ActivityStore'
+import { userInfoContext } from '../Store/UserInfoContext'
 
 const ActivityItem = (props: Activity) => {
+  const { userInfo } = useContext(userInfoContext)
+  const { location } = userInfo
+
   return (
     <div className="d-flex mb-2">
       <div>{props.description}</div>
-      <div className={`ml-auto text-${props.type === "1" ? "primary" : "danger"}`}>{props.nominal}</div>
+      <div className={`ml-auto text-${props.type === "1" ? "primary" : "danger"}`}>{location.currency}{props.nominal}</div>
     </div>
   )
 }
