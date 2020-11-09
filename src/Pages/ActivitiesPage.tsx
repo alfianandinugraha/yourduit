f3f00f8c18021e3d3baa677485c80ef917f590de
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-09 10:38:29 +0700

    add: adding background hero

--- src/Pages/ActivitiesPage.tsx
+++ src/Pages/ActivitiesPage.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { LayoutProtectedPage } from '../Layout/LayoutProtectedPage'
+import { BackgroundHero } from '../Style/Styled'
 
 export const ActivitiesPage = () => {
   return (
     <LayoutProtectedPage>
+      <BackgroundHero className="bg-primary w-100 position-absolute" height="311px"/>
     </LayoutProtectedPage>
   )
 }
