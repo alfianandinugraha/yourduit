a0dc6d85df8e740391010b7f93f282d665c7650c
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-09 17:51:02 +0700

    add: adding hamburger icon

---
+++ src/Common/HamburgerIcon.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const HamburgerIcon = () => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="22" viewBox="0 0 36 22">
+      <g id="hamburger-icon" transform="translate(-34 -13)">
+        <rect id="Rectangle_6" data-name="Rectangle 6" width="36" height="4" rx="2" transform="translate(34 13)" fill="#fff" />
+        <rect id="Rectangle_7" data-name="Rectangle 7" width="26" height="4" rx="2" transform="translate(34 22)" fill="#fff" />
+        <rect id="Rectangle_8" data-name="Rectangle 8" width="15" height="4" rx="2" transform="translate(34 31)" fill="#fff" />
+      </g>
+    </svg>
+  )
+}
