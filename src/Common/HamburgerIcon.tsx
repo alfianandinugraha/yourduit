1870aadad5214036512f576adc24d3b16c9761a4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-09 18:28:45 +0700

    add: adding all props

--- src/Common/HamburgerIcon.tsx
+++ src/Common/HamburgerIcon.tsx
@@ -1,8 +1,8 @@
 import React from 'react'
 
-export const HamburgerIcon = () => {
+export const HamburgerIcon = (props: any) => {
   return (
-    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="22" viewBox="0 0 36 22">
+    <svg xmlns="http://www.w3.org/2000/svg" width="36" height="22" viewBox="0 0 36 22" {...props}>
       <g id="hamburger-icon" transform="translate(-34 -13)">
         <rect id="Rectangle_6" data-name="Rectangle 6" width="36" height="4" rx="2" transform="translate(34 13)" fill="#fff" />
         <rect id="Rectangle_7" data-name="Rectangle 7" width="26" height="4" rx="2" transform="translate(34 22)" fill="#fff" />
