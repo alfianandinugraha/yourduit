e0184cb7ca46d29afe3e9f09fdccd0361fae9765
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-09 11:14:11 +0700

    add: adding state to update range date

--- src/Pages/ActivitiesPage.tsx
+++ src/Pages/ActivitiesPage.tsx
@@ -1,11 +1,25 @@
-import React from 'react'
+import React, { useContext } from 'react'
+import { Col, Row } from 'react-bootstrap'
+import useInputForm from '../Hooks/useInputForm'
 import { LayoutProtectedPage } from '../Layout/LayoutProtectedPage'
+import { activityContext } from '../Store/ActivityStore'
 import { BackgroundHero } from '../Style/Styled'
 
 export const ActivitiesPage = () => {
+  const { getAllActivitiesByDate } = useContext(activityContext)
+
+  const [dateFromValue, setDateFromValue] = useInputForm("")
+  const [dateToValue, setDateToValue] = useInputForm("")
+
   return (
     <LayoutProtectedPage>
-      <BackgroundHero className="bg-primary w-100 position-absolute" height="311px"/>
+      <BackgroundHero className="bg-primary w-100 position-absolute" height="311px" />
+      <Row>
+        <Col>
+          <input type="date" onChange={setDateFromValue} />
+          <input type="date" onChange={setDateToValue} />
+        </Col>
+      </Row>
     </LayoutProtectedPage>
   )
 }
