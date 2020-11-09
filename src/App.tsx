abe8418d2040f398a3b8b4f5fcc18144599a7960
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-09 17:51:16 +0700

    add: inserting hamburger icon

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
 import React from 'react';
+import { Container } from 'react-bootstrap';
 import { Route, Switch } from 'react-router-dom';
 import { ProtectedAuthRoute } from './Auth/ProtectedAuthRoute';
 import { ProtectedRoute } from './Auth/ProtectedRoute';
+import { HamburgerIcon } from './Common/HamburgerIcon';
 import { ActivitiesPage } from './Pages/ActivitiesPage';
 import { ActivityPage } from './Pages/ActivityPage';
 import DashboardPage from './Pages/DashboardPage';
@@ -11,6 +13,9 @@ import { Store } from './Store/Store';
 function App() {
   return (
     <>
+      <Container className="position-absolute" style={{top: 24}}>
+        <HamburgerIcon />
+      </Container>
       <Store>
         <Switch>
           <Route path="/dashboard">
