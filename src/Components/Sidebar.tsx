382100b6612cdc058712558ef5d292d38aee4b63
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-10 08:07:42 +0700

    add: adding item each nav

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,5 +1,9 @@
 import React, { useContext } from 'react'
 import { Col, Row } from 'react-bootstrap'
+import { Link } from 'react-router-dom'
+import { CalendarIcon } from '../Common/CalendarIcon'
+import { DollarIcon } from '../Common/DollarIcon'
+import { UserIcon } from '../Common/UserIcon'
 import { themeContext } from '../Store/ThemeStore'
 import { SidebarWrapper } from '../Style/Styled'
 import { Backdrop } from './Backdrop'
@@ -33,6 +37,24 @@ export const Sidebar = () => {
                       fontWeight: 800
                     }
                   }>yourDuit</h1>
+                </Col>
+              </Row>
+              <Row style={{ marginTop: '2.5rem'}}>
+                <Col className="col-12">
+                  <Link to="/dashboard" style={{color: '#747474'}}>
+                    <DollarIcon style={{marginRight: '1rem'}}/>
+                    <span>Dashboard</span>
+                  </Link>
+                </Col>
+                <Col className="col-12 mt-4">
+                  <Link to="/activities" style={{color: '#747474'}}>
+                    <CalendarIcon style={{ marginRight: '1rem' }} fill="#747474" height="20.2" width="18.23"/>
+                    <span>Calendar</span>
+                  </Link>
+                </Col>
+                <Col className="col-12 mt-4">
+                  <UserIcon style={{marginRight: '1rem'}}/>
+                  <span>About Creator</span>
                 </Col>
               </Row>
             </SidebarWrapper>
