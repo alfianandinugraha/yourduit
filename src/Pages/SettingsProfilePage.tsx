b22f3dc3640f68343685eee0c127c9ad4a7999b3
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-10 11:08:21 +0700

    add: adding BackgroundHero to SettingsProfilePage

--- src/Pages/SettingsProfilePage.tsx
+++ src/Pages/SettingsProfilePage.tsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import { LayoutProtectedPage } from '../Layout/LayoutProtectedPage'
+import { BackgroundHero } from '../Style/Styled'
 
 export const SettingsProfilePage = () => {
   return (
     <>
+      <BackgroundHero className="bg-primary w-100 position-absolute" height="402px"/>
       <LayoutProtectedPage disableCashSummary={true}>
-        <h1>Settings</h1>
+        <h1 className="text-white">Settings</h1>
       </LayoutProtectedPage>
     </>
   )
