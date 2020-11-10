d7ecfd894c205ebb5fabc3729dccd7cd959a2145
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-10 07:59:46 +0700

    add: adding icon Calenda, Dollar, and User

---
+++ src/Common/UserIcon.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+export const UserIcon = (props: any) => {
+  return (
+    <svg xmlns="http://www.w3.org/2000/svg" width="15.342" height="17.455" viewBox="0 0 15.342 17.455" {...props}>
+      <g id="user" transform="translate(-31)">
+        <g id="Group_82" data-name="Group 82" transform="translate(34.068)">
+          <g id="Group_81" data-name="Group 81">
+            <path id="Path_87" data-name="Path 87" d="M125.6,0a4.6,4.6,0,1,0,4.6,4.6A4.608,4.608,0,0,0,125.6,0Z" transform="translate(-121)" fill="#747474" />
+          </g>
+        </g>
+        <g id="Group_84" data-name="Group 84" transform="translate(31 10.228)">
+          <g id="Group_83" data-name="Group 83">
+            <path id="Path_88" data-name="Path 88" d="M44.4,301.984a6.561,6.561,0,0,0-4.7-1.984H37.648a6.561,6.561,0,0,0-4.7,1.984A6.707,6.707,0,0,0,31,306.716a.511.511,0,0,0,.511.511H45.83a.511.511,0,0,0,.511-.511A6.707,6.707,0,0,0,44.4,301.984Z" transform="translate(-31 -300)" fill="#747474" />
+          </g>
+        </g>
+      </g>
+    </svg>
+  )
+}
