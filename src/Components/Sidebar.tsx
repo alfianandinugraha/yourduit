8f0de03c916c454784b525783ff2f498ad5b7f87
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-10 07:36:24 +0700

    add: adding logo yourDuit

--- src/Components/Sidebar.tsx
+++ src/Components/Sidebar.tsx
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react'
+import { Col, Row } from 'react-bootstrap'
 import { themeContext } from '../Store/ThemeStore'
 import { SidebarWrapper } from '../Style/Styled'
 import { Backdrop } from './Backdrop'
@@ -15,12 +16,25 @@ export const Sidebar = () => {
               <div className="bg-primary position-absolute" style={
                 {
                   height: '142px',
-                  zIndex: 1000,
                   top: 0,
                   left: 0,
                   width: '100%'
                 }
               }></div>
+              <Row style={
+                {
+                  marginTop: '80px'
+                }
+              }>
+                <Col>
+                  <h1 className="text-white" style={
+                    {
+                      fontSize: '35px',
+                      fontWeight: 800
+                    }
+                  }>yourDuit</h1>
+                </Col>
+              </Row>
             </SidebarWrapper>
             <Backdrop onClick={() => resetActivityFormShow()}/>
           </>
