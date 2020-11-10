6c59c9dd0929353c1c083eb157f6c0eb088cdb2d
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-10 14:56:29 +0700

    add: create LiquidBackground

---
+++ src/Components/LiquidBackground.tsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { Liquid1 } from '../Common/Liquid1'
+
+interface Props {
+  top?: string;
+  left?: string;
+  bottom?: string;
+  right?: string;
+  rotate?: string;
+}
+
+export const LiquidBackground = (props: Props) => {
+  return (
+    <Liquid1 style={ 
+      {
+        cursor: 'auto',
+        position: 'absolute',
+        ...props
+      }
+    }/>
+  )
+}
