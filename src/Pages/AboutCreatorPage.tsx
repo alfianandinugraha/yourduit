c0d0bf1399ce50023237396674eabe48bb591104
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-10 12:24:30 +0700

    add: adding background hero

--- src/Pages/AboutCreatorPage.tsx
+++ src/Pages/AboutCreatorPage.tsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { LayoutProtectedPage } from '../Layout/LayoutProtectedPage'
+import { BackgroundHero } from '../Style/Styled'
 
 export const AboutCreatorPage = () => {
   return (
     <>
+      <BackgroundHero className="bg-primary w-100 position-absolute" height="232px" />
       <LayoutProtectedPage disableCashSummary={true}>
-        <h1>Hello !</h1>
       </LayoutProtectedPage>
     </>
   )
