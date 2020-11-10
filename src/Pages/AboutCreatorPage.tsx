7acd289101e8f1026b28a00fee3ea7b9fc31ec3a
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-10 12:37:32 +0700

    add: adding image

--- src/Pages/AboutCreatorPage.tsx
+++ src/Pages/AboutCreatorPage.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Col, Row } from 'react-bootstrap'
 import { LayoutProtectedPage } from '../Layout/LayoutProtectedPage'
 import { BackgroundHero } from '../Style/Styled'
 
@@ -7,6 +8,28 @@ export const AboutCreatorPage = () => {
     <>
       <BackgroundHero className="bg-primary w-100 position-absolute" height="232px" />
       <LayoutProtectedPage disableCashSummary={true}>
+        <Row>
+          <Col>
+            <div
+              className="d-flex justify-content-center"
+              style={{
+                marginTop: '3.5rem'
+              }}
+            >
+              <img 
+                src="/alfianandi-photo.jpg" 
+                alt="Alfian andi nugraha" 
+                height="168px" 
+                width="168px"
+                style={{
+                  borderRadius: '50%',
+                  border: '10px solid white',
+                  boxSizing: 'content-box'
+                }}
+              />
+            </div>
+          </Col>
+        </Row>
       </LayoutProtectedPage>
     </>
   )
