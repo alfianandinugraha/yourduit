e472c80229498c9de35a9a60ae7847d9adf69965
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-11 17:32:36 +0700

    add: adding alert after user update activity

--- src/Components/UpdateActivityForm.tsx
+++ src/Components/UpdateActivityForm.tsx
@@ -1,8 +1,9 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Activity, activityContext } from '../Store/ActivityStore'
 import { themeContext } from '../Store/ThemeStore'
 import { ActivityForm } from './ActivityForm'
 import { ActivityFormAnimation } from './ActivityFormAnimation'
+import { Alert } from './Alert'
 
 interface Props {
   idToUpdate: number;
@@ -12,9 +13,16 @@ interface Props {
 export const UpdateActivityForm = (props: Props) => {
   const { isUpdateActivityFormShow } = useContext(themeContext)
   const { updateActivity } = useContext(activityContext)
+  const [isAlertShow, setIsAlertShow] = useState(false)
+
+  useEffect(() => {
+    if (!isAlertShow) return
+    setTimeout(() => setIsAlertShow(false), 2000)
+  }, [isAlertShow, setIsAlertShow])
 
   return (
     <>
+      <Alert message="Activity has been updated" isShow={isAlertShow} variant="primary"/>
       <ActivityFormAnimation state={isUpdateActivityFormShow}>
         <ActivityForm getPayload={(payload) => {
           const { id, createdAt } = props.activity
@@ -25,6 +33,7 @@ export const UpdateActivityForm = (props: Props) => {
           }
 
           updateActivity(id, activity)
+          setIsAlertShow(true)
         }} activity={props.activity} />
       </ActivityFormAnimation>
     </>
