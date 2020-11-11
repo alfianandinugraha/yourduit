6b358882108fc7871064d99bed86115f6a9dc1b4
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-11 17:20:09 +0700

    add: adding animation Alert when user has added activity

--- src/Components/AddActivityForm.tsx
+++ src/Components/AddActivityForm.tsx
@@ -1,16 +1,25 @@
-import React, { useContext } from 'react'
+import React, { useContext, useEffect, useState } from 'react'
 import { Activity, activityContext } from '../Store/ActivityStore'
 import { ActivityForm } from './ActivityForm'
 import { saveActivityToLocalStorage } from '../Functions/LocalStorage'
 import { themeContext } from '../Store/ThemeStore'
 import { ActivityFormAnimation } from './ActivityFormAnimation'
+import { Alert } from './Alert'
+import { setTimeout } from 'timers'
 
 export const AddActivityForm = () => {
   const { addActivity } = useContext(activityContext)
   const { isAddActivityFormShow } = useContext(themeContext)
+  const [isAlertShow, setIsAlertShow] = useState(false)
+
+  useEffect(() => {
+    if (!isAlertShow) return
+    setTimeout(() => setIsAlertShow(false), 2000)
+  }, [isAlertShow, setIsAlertShow])
 
   return (
     <>
+      <Alert message="Activity has been added" isShow={isAlertShow} variant="primary"/>
       <ActivityFormAnimation state={isAddActivityFormShow}>
         <ActivityForm getPayload={(payload) => {
           const { description, nominal, timestamp, type } = payload
@@ -20,6 +29,7 @@ export const AddActivityForm = () => {
             createdAt: timestamp,
             updatedAt: timestamp,
           }
+          setIsAlertShow(true)
 
           addActivity(activity)
           saveActivityToLocalStorage(activity)
