fad84d52bd0d620352c52f542d25f3db62abcc62
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-11 15:55:26 +0700

    add: wrapping AlertWrapper with CSSTransition

--- src/Components/Alert.tsx
+++ src/Components/Alert.tsx
@@ -1,18 +1,22 @@
 import React from 'react'
+import { CSSTransition } from 'react-transition-group'
 import { AlertWrapper } from '../Style/Styled'
 
 interface Props {
   message: string;
   variant: "primary" | "danger";
+  isShow: boolean;
 }
 
 export const Alert = (props: Props) => {
   return (
-    <AlertWrapper>
-      <div className={`alert-border bg-${props.variant}`}></div>
-      <div className={`alert-content text-${props.variant}`}>
-        {props.message}
-      </div>
-    </AlertWrapper>
+    <CSSTransition timeout={300} in={props.isShow} unmountOnExit classNames="alert-slide">
+      <AlertWrapper>
+        <div className={`alert-border bg-${props.variant}`}></div>
+        <div className={`alert-content text-${props.variant}`}>
+          {props.message}
+        </div>
+      </AlertWrapper>
+    </CSSTransition>
   )
 }
