07e0f5b5304744f1bdcb40761de34799295879e7
Alfian Andi Nugraha <alfian.23andi@gmail.com>
2020-11-11 13:51:45 +0700

    add: adding animation for AddActivityForm and UpdateActivityForm

--- src/Components/AddActivityForm.tsx
+++ src/Components/AddActivityForm.tsx
@@ -3,6 +3,7 @@ import { Activity, activityContext } from '../Store/ActivityStore'
 import { ActivityForm } from './ActivityForm'
 import { saveActivityToLocalStorage } from '../Functions/LocalStorage'
 import { themeContext } from '../Store/ThemeStore'
+import { ActivityFormAnimation } from './ActivityFormAnimation'
 
 export const AddActivityForm = () => {
   const { addActivity } = useContext(activityContext)
@@ -10,22 +11,20 @@ export const AddActivityForm = () => {
 
   return (
     <>
-      {
-        !isAddActivityFormShow ? null : (
-          <ActivityForm getPayload={(payload) => {
-            const { description, nominal, timestamp, type } = payload
-            const activity: Activity = {
-              description, nominal, type,
-              id: new Date().getTime(),
-              createdAt: timestamp,
-              updatedAt: timestamp,
-            }
+      <ActivityFormAnimation state={isAddActivityFormShow}>
+        <ActivityForm getPayload={(payload) => {
+          const { description, nominal, timestamp, type } = payload
+          const activity: Activity = {
+            description, nominal, type,
+            id: new Date().getTime(),
+            createdAt: timestamp,
+            updatedAt: timestamp,
+          }
 
-            addActivity(activity)
-            saveActivityToLocalStorage(activity)
-          }} />
-        )
-      }
+          addActivity(activity)
+          saveActivityToLocalStorage(activity)
+        }} />
+      </ActivityFormAnimation>
     </>
   )
 }
--- src/Components/UpdateActivityForm.tsx
+++ src/Components/UpdateActivityForm.tsx
@@ -2,6 +2,7 @@ import React, { useContext } from 'react'
 import { Activity, activityContext } from '../Store/ActivityStore'
 import { themeContext } from '../Store/ThemeStore'
 import { ActivityForm } from './ActivityForm'
+import { ActivityFormAnimation } from './ActivityFormAnimation'
 
 interface Props {
   idToUpdate: number;
@@ -14,20 +15,18 @@ export const UpdateActivityForm = (props: Props) => {
 
   return (
     <>
-      {
-        !isUpdateActivityFormShow ? null : (
-          <ActivityForm getPayload={(payload) => {
-            const { id, createdAt } = props.activity
-            const { description, nominal, timestamp, type } = payload
-            const activity: Activity = {
-              description, nominal, type,id,createdAt,
-              updatedAt: timestamp,
-            }
+      <ActivityFormAnimation state={isUpdateActivityFormShow}>
+        <ActivityForm getPayload={(payload) => {
+          const { id, createdAt } = props.activity
+          const { description, nominal, timestamp, type } = payload
+          const activity: Activity = {
+            description, nominal, type, id, createdAt,
+            updatedAt: timestamp,
+          }
 
-            updateActivity(id, activity)
-          }} activity={props.activity}/>
-        )
-      }
+          updateActivity(id, activity)
+        }} activity={props.activity} />
+      </ActivityFormAnimation>
     </>
   )
 }
